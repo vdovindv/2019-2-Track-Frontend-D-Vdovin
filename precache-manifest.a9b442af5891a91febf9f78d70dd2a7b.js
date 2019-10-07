cb83388d09a74c8c46af758ef502907d9d7e80d4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-07 06:26:56 +0000

    Deploy vdovindv/2019-2-Track-Frontend-D-Vdovin to github.com/vdovindv/2019-2-Track-Frontend-D-Vdovin.git:gh-pages

---
+++ precache-manifest.a9b442af5891a91febf9f78d70dd2a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5485552c573a59e980cb8e741746fe10",
+    "url": "/2019-2-Track-Frontend-D-Vdovin/index.html"
+  },
+  {
+    "revision": "2d3d0f17d0e02106b53b",
+    "url": "/2019-2-Track-Frontend-D-Vdovin/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "7225b7ebdbd8af59b3a9",
+    "url": "/2019-2-Track-Frontend-D-Vdovin/static/js/2.ef3bba71.chunk.js"
+  },
+  {
+    "revision": "2d3d0f17d0e02106b53b",
+    "url": "/2019-2-Track-Frontend-D-Vdovin/static/js/main.4ef24af2.chunk.js"
+  },
+  {
+    "revision": "9ebede9078cc0f944c61",
+    "url": "/2019-2-Track-Frontend-D-Vdovin/static/js/runtime-main.661972be.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "/2019-2-Track-Frontend-D-Vdovin/static/media/logo.795fb555.svg"
+  }
+]);
